d5989efa00037e68ffaa4fe942147488e17f826f
pjhooker <lima.cityplanner@gmail.com>
2016-07-16 20:02:43 +0200

    Add files via upload

---
+++ body_end.php
@@ -0,0 +1,9 @@
+<!-- js list -->
+
+  <!-- jQuery Core & UI-->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+
+  <script src="https://npmcdn.com/leaflet@0.7.7/dist/leaflet.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="http://fezvrasta.github.io/bootstrap-material-design/dist/js/material.min.js"></script>
